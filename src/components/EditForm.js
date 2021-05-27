3f680f4cf9d786c5ad44ffe81554738b27b9dc39
lanegrimaud <lanewgrimaud@gmail.com>
2021-05-27 11:51:00 -0400

    CRUD acheived with separate components

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,71 +1,70 @@
 import React, { Component } from 'react'
 
-class App extends React.Component {
+class EditForm extends Component {
+
     render() {
         return (
-            <div>
-                <div className="movie">
-                    <h4>{this.props.movie.title}</h4>
-                    <img src={this.props.movie.image} />
-                    <p>{this.props.movie.synopsis}</p>
-                    <h4> released: {this.props.movie.year}</h4>
-                    <h4>{this.props.movie.rating}</h4>
-                    <button 
-                        value={ this.props.movie.id} 
-                        onClick={this.props.deleteMovie}
-                        >
-                        Delete Movie
+            <div className="edit">
+                <button
+                    value={this.props.movie.id}
+                    onClick={this.props.deleteMovie}
+                >
+                    Delete Movie
                     </button>
-                    <details>
-                      <summary>Edit Movie</summary>
-                      <form id={this.props.movie.id} onSubmit={this.props.updateMovie}>
+                <details>
+                    <summary>Edit Movie</summary>
+                    <form id={this.props.movie.id} onSubmit={this.props.updateMovie}>
                         <label htmlFor="title">Title</label>
                         <br />
-                        <input 
-                            type="text" 
-                            id="title" 
+                        <input
+                            required
+                            type="text"
+                            id="title"
                             onChange={this.props.handleChange} />
                         <br />
 
                         <label htmlFor="image">image</label>
                         <br />
-                        <input 
-                            type="text" 
-                            id="image" 
+                        <input
+                            required
+                            type="text"
+                            id="image"
                             onChange={this.props.handleChange} />
                         <br />
                         <br />
                         <label htmlFor="synopsis">synopsis</label>
                         <br />
                         <input
-                          type="text"
-                          id="synopsis"
-                          onChange={this.props.handleChange}
+                            required
+                            type="text"
+                            id="synopsis"
+                            onChange={this.props.handleChange}
                         />
                         <br />
                         <br />
                         <label htmlFor="year">year</label>
                         <br />
-                        <input 
-                            type="text" 
-                            id="year" 
+                        <input
+                            required
+                            type="text"
+                            id="year"
                             onChange={this.props.handleChange} />
                         <br />
                         <br />
                         <label htmlFor="rating">rating</label>
                         <br />
-                        <input 
-                            type="text" 
-                            id="rating" 
+                        <input
+                            required
+                            type="text"
+                            id="rating"
                             onChange={this.props.handleChange} />
                         <br />
                         <input type="submit" value="Update Movie" />
-                      </form>
-                    </details>
-                  </div>
+                    </form>
+                </details>
             </div>
         )
     }
 }
 
-export default App
+export default EditForm
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
+import EditForm from './EditForm'
 
 class Movie extends Component {
   render() {
     return (
       <div>
-        <h1> Movie Basement</h1>
         <div className="movie">
           <h4>{this.props.movie.title}</h4>
-          <img src={this.props.movie.image} />
+          <img src={this.props.movie.image} alt={this.props.movie.title} />
           <p>{this.props.movie.synopsis}</p>
           <h4> released: {this.props.movie.year}</h4>
           <h4>{this.props.movie.rating}</h4>
-          <button value={this.props.movie.id} onClick={this.deleteMovie}>
-            Delete Movie
-          </button>
+          <EditForm movie={this.props.movie}
+            handleChange={this.props.handleChange}
+            deleteMovie={this.props.deleteMovie}
+            updateMovie={this.props.updateMovie} />
         </div>
       </div>
     );
