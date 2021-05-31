68e407c5429f43d6adae4a53c3201118ff29d728
Kausar Akther <kausar.akther@gmail.com>
2021-05-31 12:46:45 -0400

    setup sign up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { card } from 'react'
+
+
 
 class Signup extends React.Component {
+
   render = () => {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <>
+        <card>
+            <h2>Sign Up</h2>
+            <form>
+                <div id="email">
+                    <label>Email</label>
+                    <input type="email"   required/>
+                </div>
+                <div id="password">
+                    <label>Password</label>
+                    <input type="password"   required/>
+                </div>
+                <div id="password-confirm">
+                    <label>Password Confirmation</label>
+                    <input type="email"  required/>
+                </div>
+                <button type="submit">
+                    Sign Up
+                </button>
+                
+            </form>
+        </card>
+        <div>
+            Already have an account? Log In
+        </div>
+    </>
+     
     )
   }
 }
