65f95bc65170cb6e69da0071c5cd9b5291784ff7
Kausar Akther <kausar.akther@gmail.com>
2021-06-01 09:51:48 -0400

    add form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,33 +1,67 @@
 import React, {Component} from "react";
 
 class Register extends React.Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            first_name: this.firstName,
+            last_name: this.lastName,
+            email: this.email,
+            password: this.password,
+            confirm_password: this.confirmPassword
+        }
+        console.log(data)
+    }
+
+
     render(){
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h3>Sign Up</h3>
+
+
                 <div className='form'>
                     <label>First Name</label>
-                    <input type="text" placeholder="First Name"/>
+                    <input 
+                        type="text" placeholder="First Name"
+                        onChange={e => this.firstName = e.target.value}
+                    />
                 </div>
 
                 <div className='form'>
                     <label>Last Name</label>
-                    <input type="text" placeholder="Last Name"/>
+                    <input 
+                        type="text" placeholder="Last Name"
+                        onChange={e => this.lastName = e.target.value}
+
+                    />
                 </div>
 
                 <div className='form'>
                     <label>Email</label>
-                    <input type="text" placeholder="Email"/>
+                    <input 
+                        type="text" placeholder="Email"
+                        onChange={e => this.email = e.target.value}
+
+                    />
                 </div>
 
                 <div className='form'>
                     <label>Password</label>
-                    <input type="text" placeholder="Password"/>
+                    <input 
+                        type="text" placeholder="Password"
+                        onChange={e => this.password = e.target.value}
+
+                    />
                 </div>
 
                 <div className='form'>
                     <label>Confirm Password</label>
-                    <input type="text" placeholder="Confirm Password"/>
+                    <input 
+                        type="text" placeholder="Confirm Password"
+                        onChange={e => this.confirmPassword = e.target.value}
+                    />
                 </div>
 
                 <button>Submit</button>
@@ -35,3 +69,6 @@ class Register extends React.Component {
         )
     }
 }
+
+
+export default Register
